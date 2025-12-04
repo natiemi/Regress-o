1a358d6e0a20d4010811eb1b68b6d3f6021d6272
kauanferes <kauan.feres1244@gmail.com>
2025-12-04 12:21:53 -0300

    mudei todas as coisas

--- Teste.py
+++ Teste.py
@@ -2,4 +2,21 @@ print("ola mundo")
 
 
 print("Hello World")
-da
+
+
+print("Olá Mundo")
+
+print("Bonjour le monde")
+
+
+print("Hola Mundo")
+
+
+
+print("Hallo Welt")
+
+print("Ciao mondo")
+print("こんにちは世界")
+print("안녕하세요 세계")
+print("Привет, мир")
+print("مرحبا بالعالم")
